d4b2079d9509b1c65387e187d8cd43ee39daa331
Ryan H <rhurtis@hotmail.com>
2020-07-21 02:25:48 +0000

    DIY assertion function.

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected){
+    console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");// non-identical strings
+assertEqual(1, 1);// identical numbers
+assertEqual("Lighthouse Labs", "Lighthouse Labs");// identical strings
+assertEqual(1, 2);// non-identical numbers
