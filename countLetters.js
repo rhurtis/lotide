dc86147ab3e69aabd3c34771ec7834f4738acf90
Ryan H <rhurtis@hotmail.com>
2020-07-23 01:45:01 +0000

    A function that counts the occurrences of letters in a string and stores them in an object.

---
+++ countLetters.js
@@ -0,0 +1,39 @@
+ 
+ 
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😞 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+/*
+Create a function called countLetters.
+The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
+*/
+
+const countLetters = function(string1) {
+  const results = {}; // the object that will hold the results.
+
+  for (let i = 0; i < string1.length; i++) {
+    let letter = string1.charAt(i);
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter]++;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+
+
+  return results;
+};
+
+console.log(countLetters('hello world')); // should return: { h: 1, e: 1, l: 3, o: 2, w: 1, r: 1, d: 1 }
+
+// Testing using the assertEqual function:NOT WORKING CORRECTLY but manual verification works. Will follow up.
+
+//assertEqual(countLetters('hello'),{ 'h': 1, 'e': 1, 'l': 2, 'o': 1 });
+
